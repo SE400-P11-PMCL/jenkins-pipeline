2973b9d8e000e1b93283bbcd76f0cb3779e8db3a
Vũ Đức Minh <vuducminh210503@gmail.com>
2024-11-05 23:02:47 +0700

    init project

---
+++ src/main/java/org/minh/TestController.java
@@ -0,0 +1,13 @@
+package org.minh;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    String home() {
+        return "Hello World";
+    }
+}
